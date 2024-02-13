9542590e2fbb70c7f3e7dabf460e150581b3c7d1
Jon Daw <142529123+thejondaw@users.noreply.github.com>
2024-02-12 19:04:35 -0600

    fixed ip

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["127.0.0.1/24]
   }
 
 
